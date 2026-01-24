ce4636a3a8b17d88e785fc0d2a4294915e972e2b
No-si-wq <owendejesus323@gmail.com>
2026-01-23 21:53:27 -0600

    Update PATH

--- electron/appPath.js
+++ electron/appPath.js
@@ -1,10 +1,15 @@
 const path = require("path");
 const fs = require("fs");
+const os = require("os");
 
-const PROGRAM_DATA =
-  process.env.PROGRAMDATA || "C:\\ProgramData";
+function getBaseDir() {
+  if (process.platform === "win32") {
+    return process.env.PROGRAMDATA || "C:\\ProgramData";
+  }
+  return path.join(os.homedir(), ".o2-system");
+}
 
-const APP_DIR = path.join(PROGRAM_DATA, "O2 System");
+const APP_DIR = path.join(getBaseDir(), "O2System");
 
 const PATHS = {
   root: APP_DIR,
@@ -17,11 +22,14 @@ const PATHS = {
 };
 
 function ensureAppDirs() {
-  Object.values(PATHS).forEach((dir) => {
-    if (!fs.existsSync(dir)) {
+  for (const dir of Object.values(PATHS)) {
+    try {
       fs.mkdirSync(dir, { recursive: true });
+    } catch (err) {
+      console.error(`No se pudo crear el directorio: ${dir}`, err);
+      throw err;
     }
-  });
+  }
 }
 
 module.exports = {
